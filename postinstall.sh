a2ee55c43b5e7e0a692a7f56089faff9f16da117
Mauricio Alexandre <malexandresilveira@gmail.com>
2024-07-09 10:25:45 -0300

    fix: fix post install

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-find_ios_root_path=$(find ../../../ -type d -name ios ! -path "*/node_modules/*" ! -path "*/.git/*" | head -n 1)
+find_ios_root_path=$(find ../../ -type d -name ios ! -path "*/node_modules/*" ! -path "*/.git/*" | head -n 1)
 
-default_reactnative_root_path="../../../ios"
+default_reactnative_root_path="../../ios"
 
 generate_command_by_path() {
     rm -rf ./framework/PushIOManager.xcframework && cp -Rf $( echo $1 )/framework/PushIOManager.xcframework ./framework/ || { echo "\n\n\n Error ==> PushIOManager.xcframework not found. Please copy the PushIOManager.xcframework to YOUR_APP_DIR/ios/framework/ and install package again. Follow README.md Installation instructions.<===  \n\n\n;"
